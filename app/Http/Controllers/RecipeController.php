210acaab080f77bc9dc92c508fbc78cc593360a4
Angel Garcia <angelrodriguez@ucol.mx>
2020-06-16 13:10:58 -0500

    labels reports

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -185,7 +185,7 @@ class RecipeController extends Controller
             $departure->code = $departure->recipe->code;
         });
 
-        $csvExporter->build($departures, ["order_number" => "OT", "code" => "Código", "name" => "Nombre", "lot" => "Lote", "quantity" => "Tamaño de Lote", "quantity_real" => "Cantidad Real",  "expired_date" => "Fecha de Caducidad", "production_status"=>"Estatus de Producción", "quality_status"=>"Estatus de Calidad"])->download('inventario_' . str_replace("_", "/", $departures[0]->recipe->name) . '_' . date('d_m_Y') . '.csv');
+        $csvExporter->build($departures, ["order_number" => "OT", "code" => "Código", "name" => "Nombre", "lot" => "Lote", "quantity" => "Tamaño de Lote", "quantity_real" => "Cantidad Real", "available_quantity"=>"Cantidad Disponible",  "expired_date" => "Fecha de Caducidad", "production_status"=>"Estatus de Producción", "quality_status"=>"Estatus de Calidad"])->download('inventario_' . str_replace("_", "/", $departures[0]->recipe->name) . '_' . date('d_m_Y') . '.csv');
     }
 
     public function stock()
